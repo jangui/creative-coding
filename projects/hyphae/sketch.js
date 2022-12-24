39d3d57a121c96f6596822685c0ac3355035a07e
jangui <jaime.dangui@gmail.com>
2022-12-24 19:54:23 +0100

    branching, color, and refractoring

--- projects/hyphae/sketch.js
+++ projects/hyphae/sketch.js
@@ -1,57 +1,69 @@
 let width;
 let height;
-let stems;
+let branches;
 let newNodes;
-let nodeColor;
 let wobbleFactor;
 let nodeShrinkFactor;
 let nodeStartSize;
 let branchAttempts;
+let newBranchDecreaseFactor;
 
-class Stem {
-  nodes;
+class Branch {
   id;
+  nodes;
 
-  constructor(id) {
-    this.id = id;
-    let startDirection = [random(), random()];
-    let x = int(random(width/4, 3*width/4));
-    let y = int(random(height/4, 3*height/4));
-    let startNode = new Node(x, y, startDirection, nodeStartSize)
+  constructor(id, startNode) {
     this.nodes = [startNode];
     newNodes.push(startNode);
   }
-
-  // add a new node to stem
   grow() {
     let lastNode = this.nodes[this.nodes.length-1];
-
-    // new node direction
-    let updateX = random(-wobbleFactor, wobbleFactor);
-    let updateY = random(-wobbleFactor, wobbleFactor);
-    let direction = [lastNode.direction[0]+updateX, lastNode.direction[1]+updateY];
-
-    // new node position
-    let velocity = [0,0];
-    let magnitude = sqrt(direction[0]**2 + direction[1]**2);
-    if (magnitude == 0) { return; }
-    velocity[0] = 2 * direction[0] / magnitude;
-    velocity[1] = 2 * direction[1]/ magnitude;
-    let position = [lastNode.x + velocity[0], lastNode.y + velocity[1]];
-
-    // create new node
-    let size = lastNode.size - nodeShrinkFactor;
-    let newNode = new Node(position[0], position[1], direction, size);
-    let collision = this.checkCollisions(newNode);
-    if (!collision) {
+    let newNode = this.growNode(lastNode);
+    if (!this.checkCollisions(newNode)) {
       newNodes.push(newNode);
       this.nodes.push(newNode);
     }
   }
 
-  // add a new branch
-  branch() {
+  growNode(node) {
+    // new node direction
+    let updateX = random(-wobbleFactor, wobbleFactor);
+    let updateY = random(-wobbleFactor, wobbleFactor);
+    let direction = [node.direction[0]+updateX, node.direction[1]+updateY];
 
+    // new node position
+    let velocity = [0,0];
+    let magnitude = sqrt(direction[0]**2 + direction[1]**2);
+    if (magnitude === 0) { return; }
+    velocity[0] = 2 * direction[0] / magnitude;
+    velocity[1] = 2 * direction[1]/ magnitude;
+    let position = [node.x + velocity[0], node.y + velocity[1]];
+
+    // create new node
+    let size = node.size - nodeShrinkFactor;
+    let color = node.color;
+    color[2] += 0.0025;
+    return new Node(position[0], position[1], direction, size, color);
+  }
+
+  // create a new branch
+  branch() {
+    // select random node
+    let node = random(this.nodes);
+
+    // create new node
+    let newNode = this.growNode(node);
+
+    // check collisions
+    let collision = this.checkCollisions(newNode);
+    if (!collision) {
+      newNodes.push(newNode);
+      newNode.size = node.size * newBranchDecreaseFactor;
+      // create new branch
+      let id = branches[branches.length-1].id + 1;
+      let branch = new Branch(id, newNode);
+      branches.push(branch);
+    }
   }
 
   // returns true if collision
@@ -65,19 +77,17 @@ class Stem {
     }
 
     // check collisions against nodes from other stems
-    for (let i = 0; i < stems.length; i++) {
+    for (let i = 0; i < branches.length; i++) {
       let tailNodes = 0
-      if (stems[i].id == this.id) { tailNodes = 12; }
-      let stem = stems[i];
-      for (let j = 0; j < stem.nodes.length - tailNodes; j++) {
-        let otherNode = stem.nodes[j];
-        let collision = ((otherNode.x-node.x)**2 + (otherNode.y-node.y)**2) < ((otherNode.size/2 + node.size)**2);
-        if (collision) {
+      if (branches[i].id === this.id) { tailNodes = 12; }
+      let branch = branches[i];
+      for (let j = 0; j < branch.nodes.length - tailNodes; j++) {
+        let otherNode = branch.nodes[j];
+        if (node.isColliding(otherNode)) {
           return true;
         }
       }
     }
-
     return false;
   }
 }
@@ -89,18 +99,23 @@ class Node {
   size;
   color;
 
-  constructor(x, y, direction, size) {
+  constructor(x, y, direction, size, color) {
     this.x = x;
     this.y = y;
-    this.color = nodeColor;
+    this.color = color;
     this.size = size;
     this.direction = direction;
   }
 
   draw() {
-    stroke(this.color);
+    colorMode(HSL, 100)
+    stroke(this.color[0], this.color[1], this.color[2]);
     strokeWeight(this.size);
     point(this.x, this.y);
+  }
+
+  isColliding(otherNode) {
+    return ((otherNode.x-this.x)**2 + (otherNode.y-this.y)**2) < ((otherNode.size/2 + this.size)**2);
   }
 }
 
@@ -113,16 +128,20 @@ function setup() {
   wobbleFactor = 5;
   nodeShrinkFactor = 0.01;
   nodeStartSize = 8;
-  nodeColor = 'purple';
   newNodes = [];
-  branchAttemps = 10;
+  branchAttempts = 10;
+  newBranchDecreaseFactor = 0.8;
 
-  let stemCount = 6;
-  stems = [];
-  for (let i = 0; i < stemCount; i++) {
-    stems.push(new Stem(i));
+  let startingBranchCount = 6;
+  branches = [];
+  for (let i = 0; i < startingBranchCount; i++) {
+    let startDirection = [random(), random()];
+    let x = int(random(width/4, 3*width/4));
+    let y = int(random(height/4, 3*height/4));
+    let color = [int(random(50,60)),60, 70]
+    let startNode = new Node(x, y, startDirection, nodeStartSize, color)
+    branches.push(new Branch(i, startNode));
   }
-
   // draw background
   background(220);
 }
@@ -134,12 +153,12 @@ function draw() {
   });
   newNodes = [];
 
-  stems.forEach( stem => {
-    stem.grow();
+  branches.forEach( branch => {
+    branch.grow();
   });
 
   for (let i = 0; i < branchAttempts; ++i) {
-    let stemIndex = int(random(0, stems.length));
-    stems[stemIndex].branch();
+    let branchIndex = int(random(0, branches.length));
+    branches[branchIndex].branch();
   }
 }
